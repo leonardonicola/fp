83209b025456ec04baa619634eb78f2160f0f6ea
Leonardo Nicola <leonardonicolares@gmail.com>
2024-11-02 19:23:43 -0300

    feat(types): added global generic interfaces for reusability

--- src/lib/interfaces/index.ts
+++ src/lib/interfaces/index.ts
@@ -1,3 +1,14 @@
-type ExtractArgsType<T> = T extends (...args: infer S) => any ? S : unknown;
+type GenericFn = (...args: any[]) => any;
+type Args<T extends GenericFn> = T extends (...args: infer S) => any
+  ? S
+  : never;
+type Param<T extends (param: any) => any> = T extends (param: infer P) => any
+  ? P
+  : never;
+type Return<T extends GenericFn> = T extends (...args: any[]) => infer S
+  ? S
+  : never;
+type First<T extends any[]> = T extends [infer F, ...infer _] ? F : never;
+type Last<T extends any[]> = T extends [...infer _, infer L] ? L : never;
 
-export type { ExtractArgsType };
+export type { Args, Param, GenericFn, Last, First, Return };
